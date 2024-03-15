b4da373ffcdf7f47dcc3846b7c551ec090f2b5d5
Emmett Williams <epw2921@gmail.com>
2024-03-15 00:22:49 -0400

    Completed tutorial
    Added admin account, images

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100, default='')
     description = models.TextField()
     technology = models.CharField(max_length=50, default= '')
+    image = models.FileField(upload_to="project_images/", blank=True)
